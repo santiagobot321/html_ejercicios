33dcf7b56ba9c8686772d7c727c4b43702c24754
santiagobot321 <boteroramirezsantiago@gmail.com>
2025-07-04 13:41:48 -0500

    terminando rutas finales

--- javascript/personal/honeypott/api_express.js
+++ javascript/personal/honeypott/api_express.js
@@ -1,8 +1,14 @@
-import { error } from 'console'
 import express from 'express'
+import fetch from "node-fetch"
+import cors from 'cors'
 
+
+// Creando el server con express
 const api = express()
 
+api.use(cors())
+
+// Base de datos falsa
 const usuarios = [
     {id: 1, nombre: "Pepe", passwd: "asd123"},
     {id: 2, nombre: "Juan", passwd: "coco45"},
@@ -11,6 +17,17 @@ const usuarios = [
 ]
 
 api.use(express.json())
+
+
+api.get('/api/mi-ip', async (req,res) => {
+    try {
+        const response = await fetch('https://api.ipify.org?format=json')
+        const data = await response.json()
+        res.json(data)
+    } catch (error) {
+        res.status(500).json({error:"IP no encontrada"})
+    }
+})
 
 
 // | GET    | `/api/users`     | Ver lista de usuarios              |
@@ -26,9 +43,33 @@ api.get('/api/users/:iduser',(req,res) => {
     if (nombreindividual) {
         res.json({id: nombreindividual.id, nombre: nombreindividual.nombre})
     } else {
-        res.status(404).json({error: "usuario no encontrado"})
+        res.status(404).json({error: "usuario no existe"})
     }
 })
+
+// | GET    | `/api/config`    | Configuración oculta (trampa)      |
+api.get('/api/config', (req,res) => {
+    res.json({
+    db_host: "localhost",
+    db_user: "admin",
+    db_password: "admin1234",
+    jwt_secret: "s3cr3tK3yH0n3y",
+    smtp_server: "mail.fakecorp.local",
+    smtp_pass: "emailPass!2023",
+    internal_api_key: "sk_test_51JH0n3yP0tFAK3k3y",
+    status: "operational",
+    last_backup: "2024-07-03T23:21:00Z"
+  });
+})
+
+
+// | GET    | `/api/.env`      | Archivos internos (trampa)         |
+api.get('/api/.env',(req,res) => {
+    res.type('text/plain').send(`DB_USER=admin\nDB_PASS=admin1234\nSECRET_KEY=honey123\nPORT=3000`)
+})
+
+
+
 
 // | POST   | `/api/login`     | Iniciar sesión                     |
 api.post('/api/login', (req,res) => {
@@ -66,21 +107,8 @@ api.delete('/api/users/:nombredel', (req,res) => {
     res.json({ mensaje: "Usuario eliminado con éxito" });
 })
 
-
-
+// El server escucha por el puerto 3000
 api.listen(3000, () => {
     console.log("Running server on port http://localhost:3000")
 })
 
-
-
-
-
-// | Método | Ruta             | ¿Qué simula?                       |
-// | ------ | ---------------- | ---------------------------------- |
-
-
-
-
-// | GET    | `/api/config`    | Configuración oculta (trampa)      |
-// | GET    | `/api/.env`      | Archivos internos (trampa)         |
--- javascript/personal/honeypott/server_node.js
+++ javascript/personal/honeypott/server_node.js
@@ -9,3 +9,4 @@ const server = http.createServer((request, response) => {
 server.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
+
